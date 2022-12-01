186a90c3bfa96f052b4f12a478706fe2af6530ee
johndavedosn <112868391+johndavedosn@users.noreply.github.com>
2022-12-01 13:37:29 +0100

    Delete IlistError.py

--- IlistError.py
+++
@@ -1,4 +0,0 @@
-class IntError(Exception):
-    def __init__(self, Elementt) -> None:
-        super().__init__(Elementt)
-        self.add_note("IntError : " + str(Elementt) + " is not an interger")
