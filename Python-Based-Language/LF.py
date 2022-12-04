14b1d5b28306abe07ddbeeee70d1697d4ab07e43
johndavedosn <112868391+johndavedosn@users.noreply.github.com>
2022-12-04 20:15:24 +0100

    Update LF.py

--- Python-Based-Language/LF.py
+++ Python-Based-Language/LF.py
@@ -1,21 +1,45 @@
 from DeclarationError import *
 # Useful Vars
 data = {}
+file_writer = open("db.txt", "a")
+file_reader = open("db.txt", "r")
 # Mathimatical Functions
 def add(a, b):
-    list = [a, b]
-    for i in list:
-        if i in data:
-            pass
-        else:
-            print(a + b)
-    print(data[a] + data[b])
+    if a in data:
+        print(data[a] + b)
+    elif b in data:
+        print(a + data[b])
+    elif a in data and b in data:
+        print(data[a] + data[b])
+    elif a not in data and b not in data:
+        print(a + b)
 def minus(a, b):
-    print(a - b)
+    if a in data:
+        print(data[a] - b)
+    elif b in data:
+        print(a - data[b])
+    elif a in data and b in data:
+        print(data[a] - data[b])
+    elif a not in data and b not in data:
+        print(a - b)
 def multiplicate(a, b):
-    print(a * b)
+    if a in data:
+        print(data[a] * b)
+    elif b in data:
+        print(a * data[b])
+    elif a in data and b in data:
+        print(data[a] * data[b])
+    elif a not in data and b not in data:
+        print(a * b)
 def divise(a, b):
-    print(a / b)
+    if a in data:
+        print(data[a] / b)
+    elif b in data:
+        print(a / data[b])
+    elif a in data and b in data:
+        print(data[a] / data[b])
+    elif a not in data and b not in data:
+        print(a / b)
 # Regulare Function
 def show(data):
     print(data)
@@ -25,3 +49,13 @@ def setvar(varname, varvalue):
     varindex = list(data).index(varname)
 def showvar(varname):
     print(data[varname])
+def store(Element):
+    if Element in data:
+        file_writer.write(str(data[Element]))
+    else:
+        file_writer.write(str(Element))
+def is_it_var(Element):
+    if Element in data:
+        print(True)
+    else:
+        print(False)
